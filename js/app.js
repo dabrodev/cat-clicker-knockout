3cc2759a82081b6bbd66ba0d7425149c2c6a0209
Damian Dabrowski <dabdamian@gmail.com>
2015-11-18 21:59:38 +0100

    refactor: added levels

--- js/app.js
+++ js/app.js
@@ -3,10 +3,24 @@ var ViewModel = function() {
   this.name = ko.observable('Tabby');
   this.imgSrc = ko.observable('img/cat1.jpg');
   this.imgAttribution = ko.observable('http://flicker.com/1');
+  this.nicknames = ko.observableArray([
+        { nickname:'Bert' },
+        { nickname: 'Charles' },
+        { nickname: 'Denise' }
+    ]);
+
 
   this.incrementCounter = function() {
     this.clickCount(this.clickCount() +1);
   };
+
+  this.level = ko.computed(function() {
+
+    if(this.clickCount() < 10)
+      return 'Teen';
+    else
+      return 'Older';
+  }, this);
 };
 
 ko.applyBindings(new ViewModel());
